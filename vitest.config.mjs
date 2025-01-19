cd876d77080f8a7b7115a2c077ec9398441c0bc4
Pooya Parsa <pooya@pi0.io>
2025-01-20 00:00:05 +0200

    test: increase timeout

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   test: {
     coverage: { include: ["src/**/*.ts"], exclude: ["src/capnp/*.*"] },
+    testTimeout: 10_000,
   },
   plugins: [tsconfigPaths()],
 });
